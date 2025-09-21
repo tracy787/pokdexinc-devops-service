aaad6c4cdb56f05f76ea3c355228eed13f7f0c73
tracy787 <cyberdefend904@gmail.com>
2025-09-21 22:00:45 +0530

    feat: add sample main app

---
+++ app/config.py
@@ -0,0 +1,4 @@
+import os
+
+def load_api_key():
+    return os.getenv("API_KEY")
---
+++ app/main.py
@@ -0,0 +1,5 @@
+from config import load_api_key
+
+if __name__ == "__main__":
+    print("Starting Pokｅdex DevOps Service...")
+    print("API Key:", load_api_key())
